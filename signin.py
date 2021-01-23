d05cebd251cd7b4368dc368b9c53d43a6d32b1f9
ploppy-pigeon <76797895+ploppy-pigeon@users.noreply.github.com>
2021-01-22 20:10:27 -0800

    Create signin.py

---
+++ signin.py
@@ -0,0 +1,24 @@
+def signin(loc, email, pwd):
+    """
+    Sign in and compare data.
+    File where emails are stored are param loc
+    Email is param email
+    Password is param pwd
+    Returns (True, email, pwd) if account exists and password is correct
+    Returns False if account exists but password is incorrect
+    Returns None if account does not exist
+    """
+    emails = pickle.load(open(loc, "rb"))
+    nded = True
+    for eml in emails:
+        if eml[0] == email:
+            if eml[1] == pwd:
+                nded = False
+                resp = True, email, pwd
+            else:
+                resp = False
+                nded = False
+            break
+    if nded:
+        resp = None
+    return resp
